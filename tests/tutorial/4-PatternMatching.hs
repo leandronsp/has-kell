5b1f664c18ab4710e6e3b012d29abf8e7502764f
Leandro Proença <leandronsp@gmail.com>
2021-07-31 16:12:41 +0100

    finishing website tutorial

--- tests/tutorial/4-PatternMatching.hs
+++ tests/tutorial/4-PatternMatching.hs
@@ -10,5 +10,7 @@ testPatternMatching = TestCase $
     assertEqual "ignoring with underscores" result "ad"
     let result = let (_, x:_) = (10, "abc") in x
     assertEqual "result is 'a'" result 'a'
+    let result = let abc@(a,b,c) = (1, 2, 3) in (abc,a,b,c)
+    assertEqual "pattern matching using the whole value" result ((1, 2, 3), 1, 2, 3)
 
 tests = TestList [TestLabel "testPatternMatching" testPatternMatching]
