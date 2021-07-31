526ead032d9188575c3306f1712644377cf7ca5b
Leandro Proença <leandronsp@gmail.com>
2021-07-31 13:16:44 +0100

    more test scenarios

--- tests/tutorial/Basic.hs
+++ tests/tutorial/Basic.hs
@@ -1,7 +1,16 @@
 import Test.HUnit
 
+import Data.List
+
 testBasicSum = TestCase $ assertEqual "test basicSum" (1 + 1) 2
+testBasicStr = TestCase $ assertEqual "test basicStr" "leandro" "leandro"
+testSortList = TestCase $ assertEqual "test sortList" (sort [42, 13, 22]) [13, 22, 42]
+
+testSortListOfStr = TestCase $ assertEqual "test sortListOfStr" (sort "leandro") "adelnor"
 
 tests = TestList [
-          TestLabel "testBasicSum" testBasicSum
+          TestLabel "testBasicSum" testBasicSum,
+          TestLabel "testBasicStr" testBasicStr,
+          TestLabel "testSortList" testSortList,
+          TestLabel "testSortListOfStr" testSortListOfStr
         ]
