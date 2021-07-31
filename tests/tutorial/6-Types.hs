322b72d93ca7c5fd512d3687b07eec62338e6a94
Leandro Proença <leandronsp@gmail.com>
2021-07-31 21:44:39 +0100

    more on types

--- tests/tutorial/6-Types.hs
+++ tests/tutorial/6-Types.hs
@@ -13,9 +13,17 @@ moveRight (x, y) distance = (x + distance, y)
 moveUp :: Point -> Int -> Point
 moveUp (x, y) distance = (x, y + distance)
 
+type Colour = String
+type ColourPoint = (Int, Int, Colour)
+
+moveColour :: ColourPoint -> Int -> Int -> ColourPoint
+moveColour (x, y, colour) xDistance yDistance =
+  (x + xDistance, y + yDistance, colour)
+
 testTypes = TestCase $
   do
     assertEqual "moveRight" (TypesTest.moveRight (0, 1) 1) (1, 1)
     assertEqual "moveUp" (TypesTest.moveUp (0, 1) 1) (0, 2)
+    assertEqual "moveColour" (TypesTest.moveColour (0, 1, "green") 1 2) (1, 3, "green")
 
 tests = TestList [TestLabel "testTypes" testTypes]
