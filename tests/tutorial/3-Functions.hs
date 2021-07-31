fc13ff613fc2a7fef97a9de2a3fef823232830cd
Leandro Proença <leandronsp@gmail.com>
2021-07-31 15:39:06 +0100

    add toUpper function

--- tests/tutorial/3-Functions.hs
+++ tests/tutorial/3-Functions.hs
@@ -1,8 +1,13 @@
 import Test.HUnit
 
+import Data.Char
+
 testFunctions = TestCase $
   do
     assertEqual "mapFunction" (map (+1) [1..5]) [2, 3, 4, 5, 6]
     assertEqual "filterFunction" (filter (>5) [3, 10, 7, 5, 6, 1]) [10, 7, 6]
+    let result = let square x = x * x in square 4
+    assertEqual "square result of 4 is 16" result 16
+    assertEqual "toUpper in a word" (map toUpper "leandro") "LEANDRO"
 
 tests = TestList [TestLabel "testFunctions" testFunctions]
